c2c625884e370d3708a8a81f4154427d69854945
NaohiroKobayashi <koba_nao@hotmail.co.jp>
2025-05-11 22:16:54 +0900

    say goodbye

--- hello.py
+++ hello.py
@@ -4,3 +4,8 @@ class Hello:
 
     def say_hello(self):
         return f"Hello, {self.name}!"
+    
+    # 小林が担当
+    # 機能追加：say_goodbyeメソッドを追加
+    def say_goodbye(self):
+        return f"Goodbye, {self.name}!"
